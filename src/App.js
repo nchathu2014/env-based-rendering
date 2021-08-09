043934603f6b6b89339daf66f67303c9774027b8
Chathuranga <nchathu2014@gmail.com>
2021-08-10 01:17:09 +0530

    Introducing env-cmd package for environment base configs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { REACT_APP_NAME, REACT_APP_KEY } = process.env;
+  console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          ENV: {REACT_APP_NAME} <br />
+          KEY: {REACT_APP_KEY}
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
+
 
         </a>
       </header>
